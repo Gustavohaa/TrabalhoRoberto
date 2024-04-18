2eb1d3928bf96ce701ad20d8c22517db25cab135
Gustavohaa <111452073+Gustavohaa@users.noreply.github.com>
2024-04-18 16:19:26 -0300

    Add files via upload

---
+++ Domino/src/Models/No.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class No {
+    No anterior;
+    No proximo;
+    Peca peca;
+
+    public No(Peca peca) {
+        this.peca = peca;
+        this.anterior = null;
+        this.proximo = null;
+    }
+}
